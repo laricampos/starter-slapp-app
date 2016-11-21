0ad391935859b19484ccb5ce4a84db30d1a76a07
laricampos <lcampos@samsao.co>
2016-11-21 12:30:51 -0200

    testing

--- server.js
+++ server.js
@@ -108,9 +108,8 @@ slapp.message('attachment', ['mention', 'direct_message'], (msg) => {
 // Catch-all for any other responses not handled above
 slapp.message('.*', ['direct_mention', 'direct_message'], (msg) => {
   // respond only 40% of the time
-  if (Math.random() < 0.4) {
     msg.say([':wave:', ':pray:', ':raised_hands:'])
-  }
+  
 })
 
 // attach Slapp to express server
